d839e22d0671da54f03f61c5d504bae8bf73ce19
ciminoce <ciminoce@yahoo.com.ar>
2023-07-01 11:18:16 -0300

    Agregar archivos de proyecto.

---
+++ ClaseEstatica01.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClaseEstatica01.Entidades;
+
+namespace ClaseEstatica01.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Statics Classes");
+            float celsius=20.5f;
+            float fahrenheit=ConversorDeTemperaturas.ConvertirDeCelsiusAFahrenheit(celsius);
+            Console.WriteLine(fahrenheit);
+            float fahrenheit2 = 68.5f;
+            float celsius2 = ConversorDeTemperaturas.ConvertirDeFahrenheitACelsius(fahrenheit2);
+            Console.WriteLine(celsius2);
+            float kelvin = ConversorDeTemperaturas.ConvertirDeCelsiusAKelvin(celsius);
+            Console.WriteLine(kelvin);
+            float celsius3 = 25f;
+            float fah = ConversorDeTemperaturas.ConvertirDeCelsiusAFahrenheit(celsius3);
+            Console.WriteLine(fah);
+        }
+    }
+}
---
+++ ClaseEstatica01.Entidades/ConversorDeTemperaturas.cs
@@ -0,0 +1,26 @@
+﻿namespace ClaseEstatica01.Entidades
+{
+    /// <summary>
+    /// Clase estática para convertir temperaturas de una 
+    /// escala a otra
+    /// </summary>
+    public static class ConversorDeTemperaturas
+    {
+        private const float CeroAbsoluto= 273.15f;
+        /// <summary>
+        /// Método estático para convertir de celsius a Fah
+        /// </summary>
+        /// <param name="celsius">Valor de la Temperatura a Convertir</param>
+        /// <returns>El resultado de la conversión en grados Fah</returns>
+        public static float ConvertirDeCelsiusAFahrenheit(float celsius)
+        {
+            return 1.8f * celsius + 32;
+        }
+        public static float ConvertirDeFahrenheitACelsius(float fahrenheit)
+        {
+            return  (fahrenheit - 32)/1.8f;
+        }
+        public static float ConvertirDeCelsiusAKelvin(float celsius) => celsius + CeroAbsoluto;
+        public static float ConvertirDeKelvinACelsius(float kelvin) => kelvin - CeroAbsoluto;
+    }
+}
